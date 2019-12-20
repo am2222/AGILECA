bfe6c0e184111dfe5460f34d0b9b2520e3ad9516
am2222 <asd56yu@gmail.com>
2019-12-20 01:15:19 -0500

    the init commit by Majid

---
+++ README.md
@@ -0,0 +1,43 @@
+---
+#Integrating cellular automata and discrete global grid systems: a case study into wildfire modelling
+**author:** "[`The Spatial lab`] (https://www.thespatiallab.org) "
+**date:** 11/20/2019
+
+
+[![Binder](http://mybinder.org/badge_logo.svg)](http://mybinder.org/v2/gh/nuest/sensebox-binder/master) [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.1135139.svg)](https://doi.org/10.5281/zenodo.1135139)
+
+
+## Abstract
+
+With new forms of digital spatial data driving new applications for monitoring and understanding environmental change, there are growing demands on traditional GIS tools for spatial data storage, management and processing. Discrete Global Grid System (DGGS) are methods to tessellate globe into multiresolution grids, which represent a global spatial fabric capable of storing heterogeneous spatial data, and improved performance in data access, retrieval, and analysis. While DGGS-based GIS may hold potential for next-generation big data GIS platforms, few of studies have tried to implement them as a framework for operational spatial analysis. Cellular Automata (CA) is a classic dynamic modeling framework which has been used with traditional raster data model for various environmental modeling such as wildfire modeling, urban expansion modeling and so on. The main objectives of this paper were to (i) investigate the possibility of using DGGS for running dynamic spatial analysis, (ii) evaluate CA as a generic data model for dynamic phenomena modeling within a DGGS data model and (iii) evaluate an in-database approach for CA modelling. To do so, a case study into wildfire spread modelling is developed. Results demonstrate that using a DGGS data model not only provides the ability to integrated different data sources, but also provides a framework to do spatial analysis without using geometry-based analysis. This results in a simplified architecture and common spatial fabric to support development of a wide array of spatial algorithms. While considerable work remains to be done, CA modelling within a DGGS-based GIS is a robust and flexible modelling framework for big-data GIS analysis in an environmental monitoring context.
+
+**Key Words**: _DGGS, Discrete Global Grid System, Cellular Automaton, Wildfire_ 
+**DOI**: [`10.5281/zenodo.1135140`](https://doi.org/10.5281/zenodo.1135140).
+
+## One-click execution
+
+This repo can be opened directly in [Binder](https://mybinder.org/) thanks to [this example](https://github.com/binder-examples/dockerfile-rstudio) and [Rocker](https://github.com/rocker-org/binder) by clicking on the "launch binder" button above.
+
+[![screencast of senseBox-Binder analysis in RStudio running on mybinder.org](https://media.giphy.com/media/l49JRjO65S0WQ1Kyk/giphy.gif)](https://media.giphy.com/media/l49JRjO65S0WQ1Kyk/giphy.gif)
+
+## Data and Software Availability
+
+To run the CA model several software packages were used; R (R Core Team 2019); Dplyr (Wickham et al. 2019) and dggridR (Barnes 2018). Table 1 also shows the different datasets, which were used for wildfire spread modelling. These data were converted into the DGGS data model and stored in the database table structure. A Netezza IBM database engine was used as big geo data storage platform, however any relational database system could be used. Currently, due to security-related issues it is not possible to share any connection to this database application. For this reason, a small portion of the data, which is used to run the model, is stored as CSV data format with a working script, which are accessible in the following GitHub repository: https://github.com/am2222/AGILECA
+
+|	|Dataset|	Resolution (spatial/temporal)	|Retrieved parameters|	Source/ Licence|
+|1|	Nasa Active fire data (VNIIRS)	|approximate spatial resolution of 350m/ daily 	Active fire data used for starting fire points|	https://earthdata.nasa.gov/earth-observation-data/near-real-time/firms/active-fire-data
+NRT VIIRS 375 m Active Fire product VNP14IMGT. Available on-line [https://earthdata.nasa.gov/firms]. doi: 10.5067/FIRMS/VIIRS/VNP14IMGT.NRT.001.
+Free to the user community.
+2	Copernicus Climate data	spatial resolution of these data is 0.1 degree / 1 hour	climate data including  wind speed and wind direction data	https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-land?tab=overview
+DOI: 10.24381/cds.e2161bac
+3	Canada Dem	0.0002 degree spatial resolution	Elevation	https://open.canada.ca/data/en/dataset/7
+f245e4d-76c2-4caa-951a-45d1d2051333
+Open Government Licence - Canada
+4	National land cover dataset	0.0002 degree spatial resolution	Land Cover	https://www.nrcan.gc.ca
+8	Landsat 8 Data
+(2016/05/03-2016/05/05-2016/05/12)	30 meters / 16 days	True color band composition to extract fire boundary	https://www.usgs.gov/landsat
+Landsat-7 image courtesy of the U.S. Geological Survey
+
+## References
+
+This project is licensed under Apache License, Version 2.0, see file LICENSE.
---
+++ model_test_cases.R
@@ -0,0 +1,412 @@
+
+#============================== data loading
+
+
+nbs_ltable <- read.csv(here('data\\nghb_test.txt'),sep=";", header=T, col.names=c("cid","direction","wind","hour","nb"))
+
+
+column_names <- c("wkt","dggid","i","j","bearing","alpha","dem","luse")
+lookup <- read.csv(here('data\\lookupRes20.txt'),sep="|", header=FALSE, col.names=column_names)
+
+
+#==============================model parameters
+
+tr <- 0.6 # threshold for converting a burning cell into burned cell 
+windCoef <- 1 #less value decreases the wind effect. for example 0.17 makes all the wind directions be in a same
+# range. 1 exagerates the wind effect. 
+elevCoef <- 1
+
+m <- 1
+optimizationInteraval <- 10
+newFireThershold <-3000
+#==============================
+
+wind <-filter(nbs_ltable,hour==1)%>%
+  dplyr::select("cid","nb","direction","wind")%>%
+  mutate(wind= case_when(
+    direction==1 ~ as.numeric(1/5),
+    direction==4 ~ as.numeric(5),
+    TRUE  ~ as.numeric(0)))
+#============================== 
+
+testWind <- function(maxiteration,wind,windCoef){
+  wind <- mutate(wind, wind=exp(windCoef*wind),optweight=1)
+  
+  
+  df <- dplyr::select(lookup,"dggid","wkt","i","j","dem","luse")
+  df$state <-0
+  df$sumr <- 0
+  # set some sells in fire 
+  df <- within(df, state[dggid %in% c(15099154439)] <- 1)
+  #============================== 
+  
+  #TODO: Apply other landuse weights. 
+  # you can apply vegetation density and vegetation type or ..
+  # 1 needleleaf forest 
+  # 2 tiga needleleaf
+  # 3 braodleaf evergreen forest R~ 0.0058
+  # 4, 5 deciduous evergreen 
+  # 6 Mixed Forest
+  # 8 shurbland R ~ 0.0082
+  # 10 Grassland R ~ 0.0031
+  # 14, 17, 18 wetland, urban, water ==0
+  df <- mutate(df,"luse" = case_when( luse %in% c(17,18)  ~ as.numeric(0),
+                                      luse %in% c(3,4,5,6) ~ as.numeric(0.75),
+                                      luse %in% c(1,10,14)  ~ as.numeric(0.50),
+                                      TRUE  ~ as.numeric(1)))%>% 
+    dplyr::select("dggid","wkt",state,luse,dem,sumr)%>% 
+    mutate(r0=luse)
+  
+  
+  #==============================
+  burningCells <- filter(df,state>0|sumr>0)%>%
+    dplyr::select("dggid")
+  potentialCells <- inner_join(burningCells,wind,by=c("dggid"="cid"))%>%
+    dplyr::select("dggid"=nb)%>%
+    dplyr::union(burningCells)
+  
+  
+  df2 <- filter(df,dggid %in% potentialCells$dggid)
+  
+  
+  j <- inner_join(df2,wind,by=c("dggid"="nb"))%>%
+    inner_join(df2,c("cid"="dggid"))%>%
+    mutate(elev=elevCoef*exp(atan(dem.x-dem.y)))%>%
+    mutate(stw=case_when(
+      state.y %in% c(1,2,3,4) ~ wind*optweight,
+      TRUE ~ 0
+    ))%>%
+    group_by(dggid)%>%
+    mutate(sumr1.x=case_when(
+      state.x==0 ~ sum(stw),
+      TRUE ~ 0
+    ),nburn=sum(state.y))%>% # sum must remove the burned cells first
+    ungroup()%>%
+    mutate(sumr.x=sumr.x+(m*sumr1.x/max(stw)))%>%
+    group_by(dggid)%>%
+    dplyr::select(dggid,"wkt"=wkt.x,"dem"=dem.x,"state"=state.x,"sumr"=sumr.x,nburn,"r0"=r0.x,"luse"=luse.x)%>%
+    distinct()%>%
+    mutate(state = case_when(state==0 & nburn==0 ~ as.numeric(0),
+                             state==0  & nburn>0 & sumr <tr ~ as.numeric(0),
+                             state==0  & nburn>0 & sumr >tr ~ as.numeric(1),
+                             state==1 ~ as.numeric(2),
+                             state==2  ~ as.numeric(3),
+                             state==3  ~ as.numeric(4),
+                             state==4  ~ as.numeric(4)))
+  
+  
+  
+  
+  #write.table(j, file = "test1.txt", row.names = FALSE, dec = ".", sep = "|", quote = FALSE)
+  
+  
+  #======
+  burningCells <-ungroup(j)%>% filter(state>0|sumr>0)%>%
+    dplyr::select("dggid")
+  potentialCells <- inner_join(burningCells,wind,by=c("dggid"="cid"))%>%
+    dplyr::select(nb)%>%
+    mutate(dggid=nb)%>%
+    dplyr::select(dggid)%>%
+    dplyr::union(burningCells)
+  
+  
+  newnghbs <- filter(potentialCells, !dggid %in% j$dggid)
+  
+  df2 <- filter(df,dggid %in% newnghbs$dggid)%>%
+    mutate(nburn=0)%>%
+    dplyr::union(j)
+  
+  
+  
+  # get init time 
+  
+  
+  name <- paste("wind_3_6_50it_c",windCoef)
+  
+  
+  finalresults <- mutate(j,step=0)
+  
+  for (i in 1:maxiteration) {
+    #print(i)
+    
+    if (i%%optimizationInteraval==0){
+      # let's run optimization once
+     # print("storing Data")
+      boundry <- filter(j)%>%
+        left_join(lookup,by=c("dggid","dggid"))%>%
+        dplyr::select(dggid,i,j,"wkt"=wkt.y,state)
+      finalresults <- mutate(j,step=i)%>%
+        dplyr::union(finalresults,j)
+      
+     # write.table(boundry, file =paste(i,name,"_data.txt",sep="") , row.names = FALSE, dec = ".", sep = "|", quote = FALSE)
+      
+      
+    }
+    
+    
+    
+    #elev*wind*optweight*r0.y
+    # df2nb <- filter(df2,!nburn==24 & !state==4)
+    
+    j <- inner_join(df2,wind,by=c("dggid"="nb"))%>%
+      inner_join(df2,c("cid"="dggid"))%>%
+      mutate(elev=elevCoef*exp(atan(dem.x-dem.y)))%>%
+      mutate(stw=case_when(
+        state.y %in% c(1,2,3,4) ~  wind, 
+        TRUE ~ 0
+      ))%>%
+      group_by(dggid)%>%
+      mutate(sumr1.x=case_when(
+        state.x==0 ~ sum(stw),
+        TRUE ~ 0
+      ),nburn=sum(state.y))%>% # sum must remove the burned cells first
+      ungroup()%>%
+      mutate(sumr.x=sumr.x+(m*sumr1.x/max(stw)))%>%
+      group_by(dggid)%>%
+      dplyr::select(dggid,"wkt"=wkt.x,"dem"=dem.x,"state"=state.x,"sumr"=sumr.x,nburn,"r0"=r0.x,"luse"=luse.x)%>%
+      distinct()%>%
+      mutate(state = case_when(state==0 & nburn==0 ~ as.numeric(0),
+                               state==0  & nburn>0 & sumr <tr ~ as.numeric(0),
+                               state==0  & nburn>0 & sumr >tr ~ as.numeric(1),
+                               state==1 ~ as.numeric(2),
+                               state==2  ~ as.numeric(3),
+                               state==3  ~ as.numeric(4),
+                               state==4  ~ as.numeric(4)))
+    
+    
+    
+    
+    
+    #write.table(j, file = "test1.txt", row.names = FALSE, dec = ".", sep = "|", quote = FALSE)
+    
+    
+    #======
+    burningCells <-ungroup(j)%>% filter(state>0|sumr>0)%>%
+      dplyr::select("dggid")
+    potentialCells <- inner_join(burningCells,wind,by=c("dggid"="cid"))%>%
+      dplyr::select(nb)%>%
+      mutate(dggid=nb)%>%
+      dplyr::select(dggid)%>%
+      dplyr::union(burningCells)
+    
+    
+    newnghbs <- filter(potentialCells, !dggid %in% j$dggid)
+    
+    df2 <- filter(df,dggid %in% newnghbs$dggid)%>%
+      mutate(nburn=0)%>%
+      dplyr::union(j)
+    
+    
+    
+  }
+  
+  return(finalresults)
+}
+
+#============================== 
+plotResult <- function(data){
+  sf_df = st_as_sf(data, wkt='wkt', crs = 4326)
+  # sf_df <- st_transform(sf_df,st_crs(3857))
+  # nc3_points <- sf::st_point_on_surface(sf_df)
+  # nc3_coords <- as.data.frame(sf::st_coordinates(nc3_points))
+  
+  p <- ggplot() + 
+    geom_sf(data=sf_df, aes(fill =  step), lwd=0, color=NA) +
+    # scale_fill_viridis_c(option = "inferno") + 
+    scale_fill_gradient(low="#DCDCDC", high="#708090")+
+    theme(axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          # panel.border = element_rect(colour = "black", fill=NA, size=1),
+          panel.background = element_blank(),
+          legend.position="none")
+  # +coord_sf(datum=st_crs(3857))
+  p
+  return(p)
+}
+#============================== 
+
+
+
+#==================landuse
+
+
+
+testLandUse <- function(maxiteration,wind,windCoef){
+  wind <- mutate(wind, wind=exp(windCoef*wind),optweight=1)
+  
+  
+  df <- dplyr::select(lookup,"dggid","wkt","i","j","dem","luse")
+  df$state <-0
+  df$sumr <- 0
+  # set some sells in fire 
+  df <- within(df, state[dggid %in% c(15102756420)] <- 1)
+  #============================== 
+  
+  #TODO: Apply other landuse weights. 
+  # you can apply vegetation density and vegetation type or ..
+  # 1 needleleaf forest 
+  # 2 tiga needleleaf
+  # 3 braodleaf evergreen forest R~ 0.0058
+  # 4, 5 deciduous evergreen 
+  # 6 Mixed Forest
+  # 8 shurbland R ~ 0.0082
+  # 10 Grassland R ~ 0.0031
+  # 14, 17, 18 wetland, urban, water ==0
+  df <- mutate(df,"luse" = case_when( 
+    luse %in% c(5,6) ~ as.numeric(0.2),
+    TRUE  ~ as.numeric(1)))%>% 
+    dplyr::select("dggid","wkt",state,luse,dem,sumr)%>% 
+    mutate(r0=luse)
+  
+  
+  #==============================
+  burningCells <- filter(df,state>0|sumr>0)%>%
+    dplyr::select("dggid")
+  potentialCells <- inner_join(burningCells,wind,by=c("dggid"="cid"))%>%
+    dplyr::select("dggid"=nb)%>%
+    dplyr::union(burningCells)
+  
+  
+  df2 <- filter(df,dggid %in% potentialCells$dggid)
+  
+  
+  j <- inner_join(df2,wind,by=c("dggid"="nb"))%>%
+    inner_join(df2,c("cid"="dggid"))%>%
+    mutate(elev=elevCoef*exp(atan(dem.x-dem.y)))%>%
+    mutate(stw=case_when(
+      state.y %in% c(1,2,3,4) ~ (r0.x),
+      TRUE ~ 0
+    ))%>%
+    group_by(dggid)%>%
+    mutate(sumr1.x=case_when(
+      state.x==0 ~ sum(stw),
+      TRUE ~ 0
+    ),nburn=sum(state.y))%>% # sum must remove the burned cells first
+    ungroup()%>%
+    mutate(sumr.x=sumr.x+(m*sumr1.x/max(stw)))%>%
+    group_by(dggid)%>%
+    dplyr::select(dggid,"wkt"=wkt.x,"dem"=dem.x,"state"=state.x,"sumr"=sumr.x,nburn,"r0"=r0.x,"luse"=luse.x)%>%
+    distinct()%>%
+    mutate(state = case_when(state==0 & nburn==0 ~ as.numeric(0),
+                             state==0  & nburn>0 & sumr <tr ~ as.numeric(0),
+                             state==0  & nburn>0 & sumr >tr ~ as.numeric(1),
+                             state==1 ~ as.numeric(2),
+                             state==2  ~ as.numeric(3),
+                             state==3  ~ as.numeric(4),
+                             state==4  ~ as.numeric(4)))
+  
+  
+  
+  
+  #write.table(j, file = "test1.txt", row.names = FALSE, dec = ".", sep = "|", quote = FALSE)
+  
+  
+  #======
+  burningCells <-ungroup(j)%>% filter(state>0|sumr>0)%>%
+    dplyr::select("dggid")
+  potentialCells <- inner_join(burningCells,wind,by=c("dggid"="cid"))%>%
+    dplyr::select(nb)%>%
+    mutate(dggid=nb)%>%
+    dplyr::select(dggid)%>%
+    dplyr::union(burningCells)
+  
+  
+  newnghbs <- filter(potentialCells, !dggid %in% j$dggid)
+  
+  df2 <- filter(df,dggid %in% newnghbs$dggid)%>%
+    mutate(nburn=0)%>%
+    dplyr::union(j)
+  
+  
+  
+  # get init time 
+  
+  
+  name <- paste("wind_3_6_50it_c",windCoef)
+  
+  
+  finalresults <- mutate(j,step=0)
+  
+  for (i in 1:maxiteration) {
+    #print(i)
+    
+    if (i%%optimizationInteraval==0){
+      # let's run optimization once
+      print("storing Data")
+      boundry <- filter(j)%>%
+        left_join(lookup,by=c("dggid","dggid"))%>%
+        dplyr::select(dggid,i,j,"wkt"=wkt.y,state)
+      finalresults <- mutate(j,step=i)%>%
+        dplyr::union(finalresults,j)
+      
+     # write.table(boundry, file =paste(i,name,"_data.txt",sep="") , row.names = FALSE, dec = ".", sep = "|", quote = FALSE)
+      
+      
+    }
+    
+    
+    
+    #elev*wind*optweight*r0.y
+    # df2nb <- filter(df2,!nburn==24 & !state==4)
+    
+    j <- inner_join(df2,wind,by=c("dggid"="nb"))%>%
+      inner_join(df2,c("cid"="dggid"))%>%
+      mutate(elev=elevCoef*exp(atan(dem.x-dem.y)))%>%
+      mutate(stw=case_when(
+        state.y %in% c(1,2,3,4) ~  r0.y, 
+        TRUE ~ 0
+      ))%>%
+      group_by(dggid)%>%
+      mutate(sumr1.x=case_when(
+        state.x==0 ~ sum(stw),
+        TRUE ~ 0
+      ),nburn=sum(state.y))%>% # sum must remove the burned cells first
+      ungroup()%>%
+      mutate(sumr.x=sumr.x+(m*sumr1.x/max(stw)))%>%
+      group_by(dggid)%>%
+      dplyr::select(dggid,"wkt"=wkt.x,"dem"=dem.x,"state"=state.x,"sumr"=sumr.x,nburn,"r0"=r0.x,"luse"=luse.x)%>%
+      distinct()%>%
+      mutate(state = case_when(state==0 & nburn==0 ~ as.numeric(0),
+                               state==0  & nburn>0 & sumr <tr ~ as.numeric(0),
+                               state==0  & nburn>0 & sumr >tr ~ as.numeric(1),
+                               state==1 ~ as.numeric(2),
+                               state==2  ~ as.numeric(3),
+                               state==3  ~ as.numeric(4),
+                               state==4  ~ as.numeric(4)))
+    
+    
+    
+    
+    
+   # write.table(j, file = "test1.txt", row.names = FALSE, dec = ".", sep = "|", quote = FALSE)
+    
+    
+    #======
+    burningCells <-ungroup(j)%>% filter(state>0|sumr>0)%>%
+      dplyr::select("dggid")
+    potentialCells <- inner_join(burningCells,wind,by=c("dggid"="cid"))%>%
+      dplyr::select(nb)%>%
+      mutate(dggid=nb)%>%
+      dplyr::select(dggid)%>%
+      dplyr::union(burningCells)
+    
+    
+    newnghbs <- filter(potentialCells, !dggid %in% j$dggid)
+    
+    df2 <- filter(df,dggid %in% newnghbs$dggid)%>%
+      mutate(nburn=0)%>%
+      dplyr::union(j)
+    
+    
+    
+  }
+  
+  return(finalresults)
+}
+
+
+
