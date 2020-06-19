718a25fc48b4e33a52729c602d3b1692b7f87ed5
Majid Hojati <asd56yu@yahoo.com>
2020-06-19 16:52:08 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,11 +1,14 @@
 print("installing libraries")
+
+install.packages("isoband")
 install.packages("tidyverse")
+devtools::install_github("tidyverse/ggplot2")
 install.packages("here")
 install.packages("gridExtra")
 install.packages("rgeos")
 install.packages("dplyr")
 install.packages("sf")
-devtools::install_github("tidyverse/ggplot2")
+
 install.packages("dggridR")
 system("pip3 install --upgrade wheel")
 system("pip3 install ipyrmd")
